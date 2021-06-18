1eb42db2db858d20dfea8dcf7c2b5758f849ad74
TaishiU <nbadreamteam3@gmail.com>
2021-06-18 18:14:38 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -18,20 +18,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _text = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter share'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+                Share.share(_text);
+              }
+            },
+          ),
+        ],
       ),
       body: Container(
-        child: Center(
-          child: ElevatedButton(
-            child: Text('Share'),
-            onPressed: () {
-              Share.share('This is cool!');
-            },
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.all(20.0),
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: '入力してください',
+                  labelText: 'テキスト',
+                ),
+                onSaved: (String? value) {
+                  _text = value!;
+                },
+              ),
+            ],
           ),
         ),
       ),
